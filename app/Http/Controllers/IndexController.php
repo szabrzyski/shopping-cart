eb7d3ac3b50bc28014b8860a833da48eb8273cdd
Sebastian Zabrzyski <szabrzyski@gmail.com>
2022-03-12 19:15:27 +0100

    widok listy produktów

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class IndexController extends Controller
 {
 
     public function index(Request $request)
     {
-        return view('index');
+        $products = Product::paginate(3)->appends(request()->except('page'));
+
+        return view('index')->with('results', $products);
+        
     }
 
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,42 @@
 @extends('layouts.app', ['activeMenu' => "index"])
- @push('js')
-     {{-- JS scripts --}}
- @endpush
- @section('content')
-     <div class="container mt-3 mt-md-3 mt-xl-4">
-     </div>
- @endsection
+@push('js')
+    {{-- JS scripts --}}
+@endpush
+@section('content')
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-12">
+                <h4 class="m-0">Product list</h4>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col" class="text-center">Edit</th>
+                                <th scope="col" class="text-center">Add to cart</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($results as $product)
+                                <tr>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->price }} PLN</td>
+                                    <td class="text-center"><button type="button" class="btn btn-link p-0"><i
+                                                class="fas fa-pen-to-square"></i></button></td>
+                                    <td class="text-center"><button type="button" class="btn btn-link p-0"><i
+                                                class="fas fa-cart-plus"></i></button></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        @include('layouts.paginationBar')
+    </div>
+@endsection
